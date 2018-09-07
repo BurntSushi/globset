7ebed3ace695aa89ae2bfed2c02d5ab005da1dce
Andrew Gallant <jamslam@gmail.com>
2018-09-07 14:43:59 -0400

    pkg: update brew tap to 0.10.0

--- pkg/brew/ripgrep-bin.rb
+++ pkg/brew/ripgrep-bin.rb
@@ -1,14 +1,14 @@
 class RipgrepBin < Formula
-  version '0.9.0'
+  version '0.10.0'
   desc "Recursively search directories for a regex pattern."
   homepage "https://github.com/BurntSushi/ripgrep"
 
   if OS.mac?
       url "https://github.com/BurntSushi/ripgrep/releases/download/#{version}/ripgrep-#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "36003ea8b62ad6274dc14140039f448cdf5026827d53cf24dad2d84005557a8c"
+      sha256 "32754b4173ac87a7bfffd436d601a49362676eb1841ab33440f2f49c002c8967"
   elsif OS.linux?
       url "https://github.com/BurntSushi/ripgrep/releases/download/#{version}/ripgrep-#{version}-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "2eb4443e58f95051ff76ea036ed1faf940d5a04af4e7ff5a7dbd74576b907e99"
+      sha256 "c76080aa807a339b44139885d77d15ad60ab8cdd2c2fdaf345d0985625bc0f97"
   end
 
   conflicts_with "ripgrep"
