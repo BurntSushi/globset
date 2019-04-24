03bf37ff4a29361c47843369f7d3dc5689b8fdac
Andrew Gallant <jamslam@gmail.com>
2019-04-24 17:21:38 -0400

    alloc: use jemalloc when building with musl
    It turns out that musl's allocator is slow enough to cause a fairly
    noticeable performance regression when ripgrep is built as a static
    binary with musl. We fix this by using jemalloc when building with musl.
    
    We continue to use the default system allocator in all other scenarios.
    Namely, glibc's allocator doesn't noticeably regress performance compared
    to jemalloc. But we could add more targets to this logic if other
    system allocators (macOS, Windows) prove to be slow.
    
    This wasn't necessary before because rustc recently stopped using jemalloc
    by default.
    
    Fixes #1268

--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,26 @@ mod path_printer;
 mod search;
 mod subject;
 
+// Since Rust no longer uses jemalloc by default, ripgrep will, by default,
+// use the system allocator. On Linux, this would normally be glibc's
+// allocator, which is pretty good. In particular, ripgrep does not have a
+// particularly allocation heavy workload, so there really isn't much
+// difference (for ripgrep's purposes) between glibc's allocator and jemalloc.
+//
+// However, when ripgrep is built with musl, this means ripgrep will use musl's
+// allocator, which appears to be substantially worse. (musl's goal is not to
+// have the fastest version of everything. Its goal is to be small and amenable
+// to static compilation.) Even though ripgrep isn't particularly allocation
+// heavy, musl's allocator appears to slow down ripgrep quite a bit. Therefore,
+// when building with musl, we use jemalloc.
+//
+// We don't unconditionally use jemalloc because it can be nice to use the
+// system's default allocator by default. Moreover, jemalloc seems to increase
+// compilation times by a bit.
+#[cfg(target_env = "musl")]
+#[global_allocator]
+static ALLOC: jemallocator::Jemalloc = jemallocator::Jemalloc;
+
 type Result<T> = ::std::result::Result<T, Box<::std::error::Error>>;
 
 fn main() {
